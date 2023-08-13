9057cc5a848489f603e135128b6b92314afd59a4
CopiaPiratA <aleustrsa2004@gmail.com>
2023-08-13 18:18:13 -0300

    verificaciones en el registro

--- js/sign-in.js
+++ js/sign-in.js
@@ -3,15 +3,24 @@ document.addEventListener('DOMContentLoaded', () => {
     document.getElementById('sign-in-btn').addEventListener('click',(e)=>{
         e.preventDefault();
 
+        let empty = false;
         let logueado = localStorage.setItem('logueado','true');
         const inputs = document.getElementsByTagName('input');
         const userData = [];
         for (let i = 0; i < inputs.length; i++) {
-            userData[i] = inputs[i].value;
+            if(inputs[i].value === '') {
+                alert('complete todos los campos')
+                empty = true;
+                break;
+            }
         }
-        localStorage.setItem('user-data',userData);
-        console.log(localStorage.getItem('user-data'));
-        //es posible que haya un error y no redirija correctamente.
-        window.location.href = '../index.html';
+        if (!empty) {
+            for (let i = 0; i < inputs.length; i++) {
+              userData[i] = inputs[i].value;
+            }
+          
+            localStorage.setItem('user-data', userData);
+            window.location.href = '../index.html';
+          }
     });
 });
