91b9be8ffc711e3eca9a97e90c36ad2c8769b155
matiaspercoco <matiaspercoco@gmail.com>
2023-08-23 14:42:01 -0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,11 +1,11 @@
 document.addEventListener("DOMContentLoaded", function(){
-
     //desafiate
     //si el usuario no inició sesión previamente, se lo enviará directo a login.html
     const logueado = localStorage.getItem('logueado');
-    if (logueado === 'false') {
-        window.location.href = '../login.html';
+    if (logueado === 'false' || logueado === null) {
+        window.location.href = './login.html';
     }
+    console.log(logueado)
     // desafiate
 
     document.getElementById("autos").addEventListener("click", function() {
@@ -22,22 +22,3 @@ document.addEventListener("DOMContentLoaded", function(){
     });
     console.log()
 });
-
-const emailForm = document.getElementById('login-form');
-const userEmailInput = document.getElementById('user-email');
-
-emailForm.addEventListener('submit', function(event) {
-    event.preventDefault(); // Evita que se recargue la página
-
-    const userEmail = userEmailInput.value; // Obtener el valor del correo electrónico ingresado
-
-    // Almacenar el correo electrónico en localStorage
-    localStorage.setItem('userEmail', userEmail);
-
-    alert('Correo electrónico guardado con éxito.');
-});
- // Obtener el correo electrónico almacenado en localStorage y mostrarlo en el campo de entrada
-const storedEmail = localStorage.getItem('userEmail');
-if (storedEmail) {
-userEmailInput.value = storedEmail;
-}
