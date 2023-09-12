8fa257fea27ee5a661c303f72d460ca53be74778
CopiaPiratA <aleustrsa2004@gmail.com>
2023-09-11 21:01:41 -0300

    redirección y css de product-info

--- js/product-info.js
+++ js/product-info.js
@@ -11,25 +11,26 @@ document.addEventListener('DOMContentLoaded', () => {
     fetch(urlPoducto)
     .then(response => response.json())
     .then(data => { 
+        console.log(data)
         let container = document.createElement('DIV');
         /* se crea un arreglo donde, por cada url que mande la API, se crea un elemento
             HTML img con esa url.
         */
         let imagesHtml = data.images.map(image => `<img src="${image}">`).join('');
-        // lo mismo con los productos relacionados.
+
         let relatedProductsHtml = data.relatedProducts.map(product => `
-            <div class="related-product">
+            <div class="related-product" data-product-id="${product.id}">
                 <h3>${product.name}</h3>
                 <img src="${product.image}">
             </div>
         `).join('');
-        
+
          // finalmente se muestran el pantalla todos los datos.
         container.innerHTML = `
             <h1>${data.name}</h1>
             <div id="img-container">${imagesHtml}</div>
-            <p>${data.cost} ${data.currency}</p>
-            <p>Vendidos: ${data.soldCount}</p>
+            <div id = 'aside-container'> 
+            </div>
             <h2>Descripción del producto</h2>
             <p>${data.description}</p>
             <h2>Productos similares</h2>
@@ -37,23 +38,53 @@ document.addEventListener('DOMContentLoaded', () => {
                 ${relatedProductsHtml}
             </div>
         `
-
+        document.getElementById('aside-container').innerHTML += `
+            <div id="buy-info">
+                <p class="price-and-sold">${data.cost} ${data.currency}</p>
+                <p class="price-and-sold">Vendidos: ${data.soldCount}</p>
+                <input type = 'number' placeholder = 'Cantidad' min = '1'>
+                <button>Comprar</button>
+                <button>Añadir al carrito</button>
+            </div>
+        `
         document.getElementById('container').appendChild(container);
+
+        // Al hacer clic en un producto relacionado, se almacena su ID en la variable selectedProductId.
+        document.getElementById('related-products-container').addEventListener('click', (event) => {
+            const clickedProduct = event.target.closest('.related-product');
+            if (clickedProduct) {
+                const selectedProductId = clickedProduct.getAttribute('data-product-id');
+                // se recarga la página con los datos el producto.
+                localStorage.setItem('productID',selectedProductId)
+                location.reload()
+            }
+        });
     })
     .catch(error => console.error('Error fetching data:', error));
-
+    
     //consiguiendo comentarios
     let urlComentarios = `https://japceibal.github.io/emercado-api/products_comments/${productId}.json`
     fetch(urlComentarios)
     .then(response => response.json())
     .then(data => { 
+
+        //generando estrellas
+        function starGenerator(score) {
+            const maxStars = 5; // El número máximo de estrellas
+            const fullStar = '⭐';
+            const starRating = fullStar.repeat(score);
+            return starRating;
+        }
+
         // lo mismo, usar .map para generar una lista con el comentario.
         let commentsArray = data.map(comment => 
             `<li class = 'comment'>
-                    <h4>${comment.user}</h4>
-                    <h4>${comment.dateTime}</h4>
+                    <div class ='title-container'>
+                        <h4>${comment.user}</h4>
+                        <h4>${comment.dateTime}</h4>
+                    </div>
                     <p>${comment.description}</p>
-                    <h4>Puntuación: ${comment.score}</h4>
+                    <h4>${starGenerator(comment.score)}</h4>
                 </li>`
         ).join('');
         document.getElementById('coment-container').innerHTML = commentsArray
@@ -72,11 +103,37 @@ document.addEventListener('DOMContentLoaded', () => {
         const coment = document.createElement('li');
         coment.className = 'comment'
         coment.innerHTML = `
-            <h4>${localStorage.getItem('nombreUsuario')}</h4>
-            <h4>${year}-${month}-${day}</h4>
-            <p>${input}</p>
-            <h4>Puntuación: ${select}</h4>
+            <div class = 'title-container'> 
+                <h4>${localStorage.getItem('nombreUsuario')}</h4>
+                <h4>${year}-${month}-${day}</h4>
+                <h5 id = 'edited'></h5>
+            </div>
+            <p id = 'comment-text'>${input}</p>
+            <h4>${select}</h4>
+            <button id = 'edit-comment-btn'>Editar</button>
         `
+        document.getElementById('user-text').value = '';
         document.getElementById('coment-container').appendChild(coment)
+        document.getElementById('comment-inputs-container').style.display = 'none';
+
+        //guardando en el localStorage el comentario.
+        localStorage.setItem('userComment',coment.innerHTML);
+        console.log(localStorage.getItem('userComment'))
+
+        //editar comentario
+        let editable = false;;
+        document.getElementById('edit-comment-btn').addEventListener('click',()=>{
+            document.getElementById('edited').textContent = 'editado';
+            if(!editable) {
+                document.getElementById('comment-text').contentEditable = 'true';
+                document.getElementById('edit-comment-btn').textContent = 'guardar';
+                editable = true;
+            }
+            else {
+                document.getElementById('comment-text').contentEditable = 'false';
+                document.getElementById('edit-comment-btn').textContent = 'editar';
+                editable = false;
+            }
+        })
     })
 });
